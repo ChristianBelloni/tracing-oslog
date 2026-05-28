23940147210f7e98fbdf22687a3fd083eeb2bcdb
Christian Belloni <christianbelloni@hotmail.it>
2026-05-28 16:23:23 +0200

    Update logger.rs

--- src/logger.rs
+++ src/logger.rs
@@ -146,6 +146,7 @@ where
 		let mut attr_visitor = FieldVisitor::new(&mut attributes);
 		event.record(&mut attr_visitor);
 		let mut message = String::new();
+		message = format!("{}  ", metadata.target())
 		if let Some(value) = attributes.remove("message") {
 			message = value;
 			message.push_str("  ");
